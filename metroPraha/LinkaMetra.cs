06871f5fd90009458c87bcaf95d13cdf05cbf2ef
Michaela Löffelmann <michaela.loffelmann@educhem.cz>
2023-06-12 10:51:54 +0200

    Přidejte soubory projektu.

---
+++ metroPraha/LinkaMetra.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metroPraha
+{
+    internal class LinkaMetra
+    {
+        string LinkaA = "LinkaA";
+        string LinkaB = "LinkaB";
+        string LinkaC = "LinkaC";
+        //string[] stanice = ;
+        //bool = aktivni;
+    }
+}
---
+++ metroPraha/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Vítej člověče, zde vidíš stanice metra A,B,C a budoucí stanici D");
+
+string[] staniceA =
+{
+        "Depo Hostivař", "Skalka", "Strašnická", "Želivského", "Flora", "Jiřího z Poděbrad", "Náměstí Míru", "Muzeum", "Můstek",
+        "Staroměstská", "Malostranská", "Hradčanská", "Dejvická", "Bořislavka", "Nádraží Veleslavín", "Petřiny", "Nemocnice Motol"
+};
+
+string[] staniceB =
+{
+        "Zličín", "Stodůlky", "Luka", "Lužiny", "Hůrka", "Nové Butovice", "Jironice, Radlická", "Smíchovské nádraží", "Anděl", "Karlovo náměstí",
+        "Národní třída", "Můstek", "Náměstí Republiky", "FLorenc", "Křižíkova", "Invalidova", "Palmovka", "Českomoravská", "Vysočanská", "Kolbenova",
+        "Hloubětín", "Rajská zahrada", "Černý Most"
+    };
+string[] staniceC =
+{
+        "Letňany", "Prosek", "Sřížkov", "Ládví", "Kobylisy", "nádraží Holešovice",
+        "Vltavská", "Florenc", "hlavní nádraží"," muzeum", "I.P. Pavlova", "Vyšehrad",
+        "Pražského povstání", "Pankrác", "Budějovická", "Kačerov – Depo", "Kačerov",
+        "Roztyly", "Chodov", "Opatov", "Háje "
+
+    };
+string[] staniceD =
+{"možná někdy bude :)"};
+
+Console.WriteLine("Vyber metro, které chceš zobrazit:");
+
